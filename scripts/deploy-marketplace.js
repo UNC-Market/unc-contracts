f820b5a407d21ac0f08b30bd117ba31b11fbbc2b
Blockchain Developer <55833057+bin0316@users.noreply.github.com>
2022-07-29 03:19:17 +0800

    Update deploy-marketplace.js

--- scripts/deploy-marketplace.js
+++ scripts/deploy-marketplace.js
@@ -12,100 +12,92 @@ async function main() {
   const signer = (await ethers.getSigners())[0]
   console.log('signer:', await signer.getAddress())
 
-  const deployFlag = {
-    verifyFactory: false,
-    verifyMerchant: false,
-    deployMerchantTemplate: false,
-    cloneMerchant: true,
-    deployFactory: false,
-    upgradeFactory: false,
-  };
-
+  const feeAddress = '0x0BF373dBbEe2AC7Af7028Ae8027a090EACB9b596';
+  
   /**
-   * Verify Merchant
+   *  Deploy and Verify SingleAuction
    */
-  if (deployFlag.verifyMerchant) {
-    const merchantAddress = '0x8c878d705de10B7a31C82922aFD870BC4f7d2b66';
-
-    await hre.run('verify:verify', {
-      address: merchantAddress,
-      constructorArguments: []
-    })
-
-    console.log("Merchant at: ", merchantAddress, " verified");
-  }
-
-  /**
-   * Verify SlashFactory
-   */
-  if (deployFlag.verifyFactory) {
-    const implementationAddress = '0x0d4c9de53ec60fa97328bc1de94814975ea3b03f' // SlashFactory implementation contract address
-    await hre.run('verify:verify', {
-      address: implementationAddress,
-      constructorArguments: []
-    })
-    console.log('SlashFactory Implementation contract verified')
-  }
-
-  /**
-   * Deploy Merchant Template
-   */
-  if (deployFlag.deployMerchantTemplate) {
-    const Merchant = await ethers.getContractFactory('Merchant', { signer: (await ethers.getSigners())[0] })
-
-    const merchantContract = await Merchant.deploy();
-    await merchantContract.deployed();
-    await sleep(30);
-    console.log("Merchant template deployed to: ", merchantContract.address);
-  }
-
-  /**
-   *  Deploy SlashFactory
-   */
-  if (deployFlag.deployFactory) {
-    const commonOwner = '0x172A25d57dA59AB86792FB8cED103ad871CBEf34';
-    const merchantTemplate = '0x8c878d705de10B7a31C82922aFD870BC4f7d2b66';
-    const defaultController = '0x8c4ac09b2Fd85d8Dff274a26D9b8ece2D84210d8';
-
-    const SlashFactory = await ethers.getContractFactory('SlashFactory', {
+   {   
+    const SingleAuction = await ethers.getContractFactory('SingleAuction', {
       signer: (await ethers.getSigners())[0]
     });
-    const factoryContract = await upgrades.deployProxy(SlashFactory, [commonOwner, merchantTemplate, defaultController], { initializer: 'initialize' });
-    await factoryContract.deployed()
+    const singleAuctionContract = await upgrades.deployProxy(SingleAuction, [feeAddress], { initializer: 'initialize' });
+    await singleAuctionContract.deployed()
 
-    console.log('SlashFactory proxy deployed: ', factoryContract.address)
+    console.log('SingleAuction proxy deployed: ', singleAuctionContract.address)
+
+    singleAuctionImplementation = await upgrades.erc1967.getImplementationAddress(singleAuctionContract.address);
+    console.log('SingleAuction Implementation address: ', singleAuctionImplementation)
+
+    await sleep(60);
+    // Verify SingleAuction
+    try {
+      await hre.run('verify:verify', {
+        address: singleAuctionImplementation,
+        constructorArguments: []
+      })
+      console.log('SingleAuction verified')
+    } catch (error) {
+      console.log('SingleAuction verification failed : ', error)
+    }    
   }
 
   /**
-   * Upgrade SlashFactory
+   *  Deploy and Verify SingleFixed
    */
-  if (deployFlag.upgradeFactory) {
-    const factoryAddress = "0x052314b94D8609F1F60674e239E783d0B2bFD0dC";
-
-    const SlashFactoryV2 = await ethers.getContractFactory('SlashFactory', {
+   {   
+    const SingleFixed = await ethers.getContractFactory('SingleFixed', {
       signer: (await ethers.getSigners())[0]
-    })
+    });
+    const singleFixedContract = await upgrades.deployProxy(SingleFixed, [feeAddress], { initializer: 'initialize' });
+    await singleFixedContract.deployed()
 
-    const upgradedFactoryContract = await upgrades.upgradeProxy(factoryAddress, SlashFactoryV2);
-    console.log('SlashFactory upgraded: ', upgradedFactoryContract.address)
+    console.log('SingleFixed proxy deployed: ', singleFixedContract.address)
+
+    singleFixedImplementation = await upgrades.erc1967.getImplementationAddress(singleFixedContract.address);
+    console.log('SingleFixed Implementation address: ', singleFixedImplementation)
+
+    await sleep(60);
+    // Verify SingleFixed
+    try {
+      await hre.run('verify:verify', {
+        address: singleFixedImplementation,
+        constructorArguments: []
+      })
+      console.log('SingleFixed verified')
+    } catch (error) {
+      console.log('SingleFixed verification failed : ', error)
+    }
   }
 
   /**
-   * Clone Merchant from SlashFactory
+   *  Deploy and Verify MultipleFixed
    */
-  if (deployFlag.cloneMerchant) {
-    const factoryAddress = '0x052314b94D8609F1F60674e239E783d0B2bFD0dC';
-    const merchantWallet = '0x7861e0f3b46e7C4Eac4c2fA3c603570d58bd1d97';
-    const receiveToken = '0x0000000000000000000000000000000000000000';
-    const reserved = [];
+  {   
+    const MultipleFixed = await ethers.getContractFactory('MultipleFixed', {
+      signer: (await ethers.getSigners())[0]
+    });
+    const multipleFixedContract = await upgrades.deployProxy(MultipleFixed, [feeAddress], { initializer: 'initialize' });
+    await multipleFixedContract.deployed()
 
-    const SlashFactory = await ethers.getContractFactory('SlashFactory', { signer: (await ethers.getSigners())[0] });
-    const slashFactory = await SlashFactory.attach(factoryAddress);
+    console.log('MultipleFixed proxy deployed: ', multipleFixedContract.address)
 
-    const tx = await slashFactory.deployMerchant(merchantWallet, receiveToken, reserved);
-    await tx.wait();
-    console.log('Merchant Cloned');
+    multipleFixedImplementation = await upgrades.erc1967.getImplementationAddress(multipleFixedContract.address);
+    console.log('MultipleFixed Implementation address: ', multipleFixedImplementation)
+
+    await sleep(60);
+    // Verify MultipleFixed
+    try {
+      await hre.run('verify:verify', {
+        address: multipleFixedImplementation,
+        constructorArguments: []
+      })
+      console.log('MultipleFixed verified')
+    } catch (error) {
+      console.log('MultipleFixed verification failed : ', error)
+    }   
   }
+
 }
 
 main()
