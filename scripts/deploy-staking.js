8a6abb96139c8ca76f6d01ca62a4d8b9f74879c8
Blockchain Developer <55833057+bin0316@users.noreply.github.com>
2022-07-30 00:56:09 +0800

    Update deploy-staking.js

--- scripts/deploy-staking.js
+++ scripts/deploy-staking.js
@@ -12,99 +12,77 @@ async function main() {
   const signer = (await ethers.getSigners())[0]
   console.log('signer:', await signer.getAddress())
 
-  const deployFlag = {
-    verifyFactory: false,
-    verifyMerchant: false,
-    deployMerchantTemplate: false,
-    cloneMerchant: true,
-    deployFactory: false,
-    upgradeFactory: false,
-  };
+  const feeAddress = process.env.FEE_ADDRESS;
 
   /**
-   * Verify Merchant
+   * Deploy SingleNFTStaking Template
    */
-  if (deployFlag.verifyMerchant) {
-    const merchantAddress = '0x8c878d705de10B7a31C82922aFD870BC4f7d2b66';
+  const SingleNFTStaking = await ethers.getContractFactory('SingleNFTStaking', { signer: (await ethers.getSigners())[0] })
 
+  const singleNFTStakingContract = await SingleNFTStaking.deploy();
+  await singleNFTStakingContract.deployed();
+  await sleep(30);
+  console.log("SingleNFTStaking template deployed to: ", singleNFTStakingContract.address);
+
+  // Verify SingleNFTStaking Template
+  try {
     await hre.run('verify:verify', {
-      address: merchantAddress,
+      address: singleNFTStakingContract.address,
       constructorArguments: []
     })
-
-    console.log("Merchant at: ", merchantAddress, " verified");
+    console.log('SingleNFTStaking verified')
+  } catch (error) {
+    console.log('SingleNFTStaking verification failed : ', error)
   }
 
+
   /**
-   * Verify SlashFactory
+   * Deploy MultiNFTStaking Template
    */
-  if (deployFlag.verifyFactory) {
-    const implementationAddress = '0x0d4c9de53ec60fa97328bc1de94814975ea3b03f' // SlashFactory implementation contract address
+  const MultiNFTStaking = await ethers.getContractFactory('MultiNFTStaking', { signer: (await ethers.getSigners())[0] })
+
+  const multiNFTStakingContract = await MultiNFTStaking.deploy();
+  await multiNFTStakingContract.deployed();
+  await sleep(30);
+  console.log("MultiNFTStaking template deployed to: ", multiNFTStakingContract.address);
+
+  // Verify MultiNFTStaking Template
+  try {
     await hre.run('verify:verify', {
-      address: implementationAddress,
+      address: multiNFTStakingContract.address,
       constructorArguments: []
     })
-    console.log('SlashFactory Implementation contract verified')
+    console.log('MultiNFTStaking verified')
+  } catch (error) {
+    console.log('MultiNFTStaking verification failed : ', error)
   }
 
   /**
-   * Deploy Merchant Template
-   */
-  if (deployFlag.deployMerchantTemplate) {
-    const Merchant = await ethers.getContractFactory('Merchant', { signer: (await ethers.getSigners())[0] })
-
-    const merchantContract = await Merchant.deploy();
-    await merchantContract.deployed();
-    await sleep(30);
-    console.log("Merchant template deployed to: ", merchantContract.address);
-  }
-
-  /**
-   *  Deploy SlashFactory
-   */
-  if (deployFlag.deployFactory) {
-    const commonOwner = '0x172A25d57dA59AB86792FB8cED103ad871CBEf34';
-    const merchantTemplate = '0x8c878d705de10B7a31C82922aFD870BC4f7d2b66';
-    const defaultController = '0x8c4ac09b2Fd85d8Dff274a26D9b8ece2D84210d8';
-
-    const SlashFactory = await ethers.getContractFactory('SlashFactory', {
+  *  Deploy and Verify NFTStakingFactory
+  */
+  {
+    const NFTStakingFactory = await ethers.getContractFactory('NFTStakingFactory', {
       signer: (await ethers.getSigners())[0]
     });
-    const factoryContract = await upgrades.deployProxy(SlashFactory, [commonOwner, merchantTemplate, defaultController], { initializer: 'initialize' });
-    await factoryContract.deployed()
+    const nftStakingFactoryContract = await upgrades.deployProxy(NFTStakingFactory, [feeAddress, singleNFTStakingContract.address, multiNFTStakingContract.address], { initializer: 'initialize' });
+    await nftStakingFactoryContract.deployed()
 
-    console.log('SlashFactory proxy deployed: ', factoryContract.address)
-  }
+    console.log('NFTStakingFactory proxy deployed: ', nftStakingFactoryContract.address)
 
-  /**
-   * Upgrade SlashFactory
-   */
-  if (deployFlag.upgradeFactory) {
-    const factoryAddress = "0x052314b94D8609F1F60674e239E783d0B2bFD0dC";
+    nftStakingFactoryImplementation = await upgrades.erc1967.getImplementationAddress(nftStakingFactoryContract.address);
+    console.log('NFTStakingFactory Implementation address: ', nftStakingFactoryImplementation)
 
-    const SlashFactoryV2 = await ethers.getContractFactory('SlashFactory', {
-      signer: (await ethers.getSigners())[0]
-    })
-
-    const upgradedFactoryContract = await upgrades.upgradeProxy(factoryAddress, SlashFactoryV2);
-    console.log('SlashFactory upgraded: ', upgradedFactoryContract.address)
-  }
-
-  /**
-   * Clone Merchant from SlashFactory
-   */
-  if (deployFlag.cloneMerchant) {
-    const factoryAddress = '0x052314b94D8609F1F60674e239E783d0B2bFD0dC';
-    const merchantWallet = '0x7861e0f3b46e7C4Eac4c2fA3c603570d58bd1d97';
-    const receiveToken = '0x0000000000000000000000000000000000000000';
-    const reserved = [];
-
-    const SlashFactory = await ethers.getContractFactory('SlashFactory', { signer: (await ethers.getSigners())[0] });
-    const slashFactory = await SlashFactory.attach(factoryAddress);
-
-    const tx = await slashFactory.deployMerchant(merchantWallet, receiveToken, reserved);
-    await tx.wait();
-    console.log('Merchant Cloned');
+    await sleep(60);
+    // Verify NFTStakingFactory
+    try {
+      await hre.run('verify:verify', {
+        address: nftStakingFactoryImplementation,
+        constructorArguments: []
+      })
+      console.log('NFTStakingFactory verified')
+    } catch (error) {
+      console.log('NFTStakingFactory verification failed : ', error)
+    }
   }
 }
 
